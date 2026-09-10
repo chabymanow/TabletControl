6ad733c0797c447b748ec0ab440328cc282e3fe7
Chaby Manow <chabymanow@gmail.com>
2026-09-10 15:01:46 +0100

    Bump Android version to 0.1.2

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.chaby.tabletcontrol"
         minSdk = 28
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

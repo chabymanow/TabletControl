e9226119a3aea143257b9e7df8dc06e33f315485
Chaby Manow <chabymanow@gmail.com>
2026-09-10 12:49:42 +0100

    Increment Android version code for 0.1.1

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.chaby.tabletcontrol"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
